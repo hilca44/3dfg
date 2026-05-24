be7880c06752abcea96937a1b50f643ebd129e51
hilca44 <113212346+hilca44@users.noreply.github.com>
2026-05-24 07:43:56 +0000

    update

--- public/schreinertool/suggest.js
+++ public/schreinertool/suggest.js
@@ -36,6 +36,116 @@ const parameterOptionsByProperty = {
   ],
 }
 
+
+
+// const parameterOptionsByProperty = {
+//   hoch: [
+//     ["1", "Material 1"],
+//     ["2", "Material 2"],
+//     ["3", "Material 3"]
+//   ],
+//   mat: [
+//     ["1", "Material 1"],
+//     ["2", "Material 2"],
+//     ["3", "Material 3"]
+//   ],
+//   o: [
+//     ["9", "9 Grad, z-Achse default"],
+//     ["45", "45 Grad, z-Achse default"],
+//     ["90", "90 Grad, z-Achse default"],
+//     ["-45", "-45 Grad, z-Achse default"],
+//     ["45x", "45 Grad um x"],
+//     ["45y", "45 Grad um y"],
+//     ["45z", "45 Grad um z"]
+//   ],
+//   u: [
+//     ["2f", "2 cm vorne"],
+//     ["2b", "2 cm hinten"],
+//     ["2f,1b", "vorne und hinten"],
+//     ["8g", "8 cm unten"]
+//   ],
+//   x: defaultParameterOptions,
+//   y: defaultParameterOptions,
+//   z: defaultParameterOptions,
+//   wdh: [
+//     ["1", "1 cm"],
+//     ["2", "2 cm"],
+//     ["5", "5 cm"],
+//     ["10", "10 cm"]
+//   ],
+//   w: [
+//     ["40", "40 cm"],
+//     ["60", "60 cm"],
+//     ["80", "80 cm"],
+//     ["120", "120 cm"]
+//   ],
+//   breit: [
+//     ["40", "40 cm"],
+//     ["60", "60 cm"],
+//     ["80", "80 cm"],
+//     ["120", "120 cm"]
+//   ],
+//   d: [
+//     ["30", "30 cm"],
+//     ["40", "40 cm"],
+//     ["60", "60 cm"]
+//   ],
+//   tief: [
+//     ["30", "30 cm"],
+//     ["40", "40 cm"],
+//     ["60", "60 cm"]
+//   ],
+//   hoch: [
+//     ["30", "30 cm"],
+//     ["72", "72 cm"],
+//     ["100", "100 cm"],
+//     ["200", "200 cm"]
+//   ],
+//   s: [
+//     ["1.6", "16 mm"],
+//     ["1.9", "19 mm"],
+//     ["2.5", "25 mm"],
+//     ["3.8", "38 mm"]
+//   ],
+//   stk: [
+//     ["1.6", "16 mm"],
+//     ["1.9", "19 mm"],
+//     ["2.5", "25 mm"],
+//     ["3.8", "38 mm"]
+//   ],
+//   sx: [
+//     ["2", "in 2 Teile"],
+//     ["3", "in 3 Teile"],
+//     ["3,5", "3 Teile, Abstand 5"]
+//   ],
+//   sy: [
+//     ["2", "in 2 Teile"],
+//     ["3", "in 3 Teile"],
+//     ["2,1", "2 Teile, Abstand 1"]
+//   ],
+//   sz: [
+//     ["2", "in 2 Teile"],
+//     ["4,2", "4 Teile, Abstand 2"]
+//   ],
+//   co: [
+//     ["white", "White"],
+//     ["wheat", "Wheat"],
+//     ["cornflowerblue", "Cornflower Blue"],
+//     ["gray", "Gray"]
+//   ],
+//   n: [
+//     ["2", "2 Wiederholungen"],
+//     ["3", "3 Wiederholungen"],
+//     ["4", "4 Wiederholungen"]
+//   ],
+//   nz: [
+//     ["2", "2 in z"],
+//     ["3", "3 in z"],
+//     ["4", "4 in z"]
+//   ]
+// };
+
+
 export const materialSuggest = {
   thickness: [
     ["16", "16 mm"],
